6c02f4a12fe04335fd529140e23b7d8bf45a5c72
TheFrenchGuy <30523707+TheFrenchGuy@users.noreply.github.com>
2021-02-16 11:52:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import string  ##Necessary for ascii to cipher
 
-letterGoodness = dict(zip(string.ascii_uppercase,
+letterPercentage = dict(zip(string.ascii_uppercase,
                           [.0817, .0149, .0278, .0425, .1270, .0223, .0202,
                            .0609, .0697, .0015, .0077, .0402, .0241, .0675,
                            .0751, .0193, .0009, .0599, .0633, .0906, .0276,
@@ -17,15 +17,15 @@ def hex_to_ascii(hex_str):  # First translate the hex to ascii
     return ascii_str
 
 
-def goodness(msg):  ##So that it returns the most english message (HAHAHAH so funny because its me)
-    return sum(letterGoodness.get(char, 0) for char in msg)  ##Evalutate all of the char each by one
+def percentage(msg):  ##So that it returns the most english message (HAHAHAH so funny because its me)
+    return sum(letterPercentage.get(char, 0) for char in msg)  ##Evalutate all of the char each by one
 
 
 def shiftBy(msg):
     msg = msg.upper()  ##So that all the characters have been upcase
     for trans_table in translatedTable:
         txt = msg.translate(trans_table)
-        yield goodness(txt), txt
+        yield percentage(txt), txt
 
 
 hex = '47 79 20 6c 67 78 20 67 79 20 63 6b 20 71 74 75 63 20 7a 6e 6b 78 6b 20 6f 79 20 74 75 20 4d 20 6f 74 20 6e 6b 64 6f 6a 6b 69 6f 73 67 72'  # The Hex you have provided to us!
